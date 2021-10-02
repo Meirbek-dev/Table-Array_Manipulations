c98021fa041143d475200d775cb1f80f470a3a99
Meirbek-dev <meirbek.b2k@gmail.com>
2021-10-03 00:09:33 +0600

    Initial commit

--- UI/main.py
+++ UI/main.py
@@ -21,99 +21,55 @@ class Main(QDialog):
         row = col = 0
         while row < self.tableWidget.rowCount():
             while col < self.tableWidget.columnCount():
-                random_num = randint(0, 101)  # Присвоение перемнной случайных значений от 0 до 100
+                random_num = randint(0, 100)  # Присвоение перемнной случайных значений от 0 до 100
                 self.tableWidget.setItem(row, col, QTableWidgetItem(str(random_num)))
                 self.tableWidget.item(row, col).text()
                 col += 1
             row += 1
             col = 0
-
-        list_information_zero_num = get_info(self.tableWidget)
-        if not list_information_zero_num:
-            self.label_status.setStyleSheet('color: red; font: bold 14pt "Calibri"')
-            self.label_status.setText('Введены неправильные данные!')
+        zero_counter = get_number_of_zeros(self.tableWidget)
+        if not zero_counter:
+            self.label_zero_amount.setText('Количество нулей: 0')
+            self.label_status.setStyleSheet('color: gray; font: bold 14pt "Calibri"')
+            self.label_status.setText('В таблице нет нулей!')
         else:
-            self.label_zero_amount.setText('Количество нулей: ' + str(list_information_zero_num[0]))
-            self.label_zero_index.setText('Индекс нулей: ' + ' [' +
-                                          str(list_information_zero_num[1]) + ';' +
-                                          str(list_information_zero_num[2]) + ']')
+            self.label_zero_amount.setText('Количество нулей: ' + str(zero_counter))
             self.label_status.setText('')
 
     def solve(self):
-
-        list_information_zero_num = get_info(self.tableWidget)
-        if not list_information_zero_num:
+        zero_counter = get_number_of_zeros(self.tableWidget)
+        if not zero_counter:
+            self.label_zero_amount.setText('Количество нулей: 0')
             self.label_status.setStyleSheet('color: red; font: bold 14pt "Calibri"')
             self.label_status.setText('В таблице отсутствуют нули!')
         else:
             self.label_status.setStyleSheet('color: green; font: bold 14pt "Calibri"')
             self.label_status.setText('Задание выполнено!')
-            self.label_zero_amount.setText('Количество нулей: ' + str(list_information_zero_num[0]))
-            self.label_zero_index.setText('Индекс нулей: ' + ' [' +
-                                          str(list_information_zero_num[1]) + ';' +
-                                          str(list_information_zero_num[2]) + ']')
-            row = 0
-            col = 0
-            flag = False
+            row = col = 0
             while row < self.tableWidget.rowCount():
                 while col < self.tableWidget.columnCount():
-                    item = self.tableWidget.item(row, col).text()
-                    # три случая:
-                    # 1) элемент равен нулю
-                    # 2) элемент равен максимальному числу
-                    #   2.1) максимальный элемент раплолагается в 1-ой ячейке и перед ним нет ячеек
-                    # 3) элемент равен иному числу
-
-                    # for i in range(len(array)):
-                    #     for j in range(len(array[i])):
-                    #         if array[i][j] % 2 != 0:
-                    #             array[i][j] = zero
-                    # return array
-
-                    if int(item) == 0:
-                        col += 1
-                    elif int(item) == list_information_zero_num[0]:
-                        if int(item) % 2 != 0:
-                            self.tableWidget.setItem(QTableWidgetItem(str(list_information_zero_num[0])))
-                        else:
-                            self.tableWidget.setItem(QTableWidgetItem(str(list_information_zero_num[0])))
-                        self.label_odds.setText('Количество нечётных элементов: ' + str(list_information_zero_num[0]))
-                        flag = True
-                        break
-                    else:
-                        self.label_odds.setText('Количество нечётных элементов: 0')
-                        flag = True
-                        break
-                if flag:
-                    break
-                row += 1
+                    number = self.tableWidget.item(row, col).text()
+                    if int(number) % 2 != 0:
+                        self.tableWidget.setItem(row, col, QTableWidgetItem(str(zero_counter)))
+                    col += 1
                 col = 0
-            self.label_status.setText('')
+                row += 1
 
 
-def get_info(table_widget):
+def get_number_of_zeros(table_widget):
     # zero_counter = sum([i.count(0) for i in array])
-    zero_counter = odd_counter = row = col = row_zero_number = col_zero_number = 0
-    # zero_num = int(table_widget.item(row_zero_number, col_zero_number).text())
-
     try:
+        zero_counter = row = col = 0
         while row < table_widget.rowCount():
             while col < table_widget.columnCount():
                 number = int(table_widget.item(row, col).text())
                 if number == 0:
-                    # zero_num = number
-                    row_zero_number = row
-                    col_zero_number = col
                     zero_counter += 1
                 col += 1
-                # elif number == 0:
-                #     odd_counter += 1
-
             row += 1
             col = 0
-        return [zero_counter, row_zero_number, col_zero_number]
+        return zero_counter
     except Exception:
-        self.label_zero_amount.setText('В таблице отсутствуют нули!')
         return None
 
 
